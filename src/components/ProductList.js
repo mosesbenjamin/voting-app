53d659f0dc30793418a1ba468dcffd1d45ca182a
El-Mavewrick <60680552+El-Mavewrick@users.noreply.github.com>
2020-05-21 08:41:51 +0100

    bug fix

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -37,7 +37,7 @@ class ProductList extends React.Component{
 
 	}
 	render(){
-			this.props.state.sort((a, b)=>{
+			this.state.products.sort((a, b)=>{
 									return b.likes - a.likes}
 									)
 			const renderedProduct = this.state.products.map((product)=>{
